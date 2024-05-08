7c9c55fe09909808dda0c9ead42db9f555bc5ad2
pradhyumnas99 <145165572+pradhyumnas99@users.noreply.github.com>
2024-05-08 11:31:19 -0400

    Revert "Immunization Fixed...."
    This reverts commit 4586f4305ef177af483cb920dad4d3c596c33a01.

--- src/main/java/edu/neu/csye6200/daycare/view/ViewAllImmunization.java
+++ src/main/java/edu/neu/csye6200/daycare/view/ViewAllImmunization.java
@@ -8,8 +8,6 @@ import edu.neu.csye6200.daycare.model.ImmunizationTracker;
 import edu.neu.csye6200.daycare.model.Student;
 import edu.neu.csye6200.daycare.repositories.ImmunizationTrackerRepository;
 
-import java.awt.event.ActionEvent;
-
 /**
  *
  * @author dhairya
@@ -37,7 +35,6 @@ public class ViewAllImmunization extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -46,7 +43,7 @@ public class ViewAllImmunization extends javax.swing.JFrame {
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
-        ImmunizationTracker result = ImmunizationTrackerRepository.findByStudentId(student.getId());
+        ImmunizationTracker result= ImmunizationTrackerRepository.findByStudentId(student.getId());
         String[] csvData = result.getImmunizationDetails().split(",");
         StringBuilder stringBuilder = new StringBuilder();
         String immName = "Hib1,Hib2,Hib3,Hib4,DTap1,DTap2,DTap3,DTap4,HepatitisB1,HepatitisB2,HepatitisB3,MMR1,Varicella";
@@ -55,34 +52,20 @@ public class ViewAllImmunization extends javax.swing.JFrame {
         for (int i = 0; i < csvData.length; i++) {
             data1 = (stringBuilder.append(immNameArray[i]).append(" ").append(csvData[i]).append("\n")).toString();
         }
-        String data3 = "Immunization Details: \n" + data1;
-        jTextArea1.setText(data3);
-
-        jButton2.setText("Exit");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
+        //ImmunizationTracker immunizationTracker = ImmunizationTrackerRepository.findByStudentId(student.getId());
+        jTextArea1.setText(data1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(281, 281, 281)
-                                .addComponent(jLabel1))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(315, 315, 315)
-                                .addComponent(jButton2)))
-                        .addGap(0, 283, Short.MAX_VALUE)))
+                .addGap(281, 281, 281)
+                .addComponent(jLabel1)
+                .addContainerGap(289, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -92,26 +75,15 @@ public class ViewAllImmunization extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 323, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(jButton2)
-                .addContainerGap(64, Short.MAX_VALUE))
+                .addContainerGap(104, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        dispose();
-
-        LoginPageLayout loginPageLayout = LoginPageLayout.getInstance();
-        loginPageLayout.setVisible(true);
-    }//GEN-LAST:event_jButton2ActionPerformed
-
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
